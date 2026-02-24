30158ec12d72575a688ddd1620db30bbb187b622
hungvu <hungvu19061991@gmail.com>
2026-02-24 16:51:19 +0700

    chuan hoa env

--- src/main/java/mdm_service/masterdata/MasterDataApplication.java
+++ src/main/java/mdm_service/masterdata/MasterDataApplication.java
@@ -1,5 +1,6 @@
 package mdm_service.masterdata;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MasterDataApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MasterDataApplication.class, args);
 	}
 
